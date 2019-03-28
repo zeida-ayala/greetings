c12ab6a0fd0f5c5c505dd9a0abba27f08aa3d232
Zeida Ayala <zeida.ayala.jala@gmail.com>
2019-03-28 13:04:12 -0400

    Added initial components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import MultiLineText from './MultiLineText.js'
+import Dropdown from './Dropdown.js'
 import './App.css';
 
 class App extends Component {
@@ -7,19 +8,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <div className="App-content">
+          <MultiLineText />
+          <Dropdown />
+        </div>
       </div>
     );
   }
---
+++ src/MultiLineText.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class MultiLineText extends Component {
+  render() {
+    return (
+        <textarea 
+        name="Text1" 
+        cols="40" 
+        rows="5">
+        </textarea>
+    )
+  }
+}
+
+export default MultiLineText;
